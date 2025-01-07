02b3bb597bcfb78ab045016d78b39da25a16b4fe
gunnarebeling <gunnarebeling@icloud.com>
2025-01-07 11:09:15 -0600

    added comments model

---
+++ Models/Comment.cs
@@ -0,0 +1,12 @@
+namespace Tabloid.Models;
+
+    public class Comment
+{
+    public int Id { get; set; }
+    public int PostId { get; set; }
+    public int UserProfileId { get; set; }
+    public UserProfile UserProfile { get; set; }
+    public string Subject { get; set; }
+    public string Content { get; set; }
+    public DateTime CreationDate { get; set; }
+}
