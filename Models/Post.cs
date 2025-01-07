0f6b0491696362ce7b6aa1f70f68444ed29f801b
Taylor Albright <jtayloralbright@gmail.com>
2025-01-07 11:35:46 -0600

    edit

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace Tabloid.Models;
 
 public class Post 
@@ -5,8 +7,10 @@ public class Post
     public int Id { get; set; }
     public string Title { get; set; }
     public int UserProfileId { get; set; }
+    public UserProfile UserProfile { get; set; }
     public string Content { get; set; }
     public int CategoryId { get; set; }
+    public Category Category { get; set; }
     public string HeaderImageUrl { get; set; }
     public DateTime PublicationDate { get; set; }
     public TimeSpan ReadTime { get; set; }
